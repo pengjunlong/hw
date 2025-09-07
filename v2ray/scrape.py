d0d0718d17cc352a441e4df77afa52323d197c26
pengjunlong <pengjunlong@meituan.com>
2025-09-07 21:41:52 +0800

    v2rayshareorg

--- v2ray/scrape.py
+++ v2ray/scrape.py
@@ -1,8 +1,5 @@
 import base64
-import urllib.parse
 from datetime import datetime
-import socket
-from typing import Dict, Union
 
 import requests
 from bs4 import BeautifulSoup
@@ -20,6 +17,7 @@ def fetch_text(url):
         print(url + " base64 decode failed")
     if not text.endswith('\n'):
         text += '\n'
+    print(text)
     return text
 
 
@@ -41,8 +39,72 @@ def aiboboxx():
     return text
 
 
-def ermaozi():
-    url = "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt"
+def miluonode():
+    today = datetime.today()
+    url = f"http://miluonode.cczzuu.top/node/{today.strftime('%Y%m%d')}-v2ray.txt"
+    text = fetch_text(url)
+    return text
+
+
+def freeclash():
+    today = datetime.today()
+    url = f"https://free-clash-v2ray.github.io/uploads/{today.year}/{today.month:02d}/0-{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def freeclashnode():
+    today = datetime.today()
+    url = f"https://node.freeclashnode.com/uploads/{today.year}/{today.month:02d}/0-{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def vpnlogin():
+    today = datetime.today()
+    url = f"https://vpnlogin.github.io/uploads/{today.year}/{today.month:02d}/0-{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def v2rayshareorg():
+    today = datetime.today()
+    url = f"https://node.v2rayshare.org/uploads/{today.year}/{today.month:02d}/0-{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def v2rayclashfree():
+    today = datetime.today()
+    url = f"https://v2rayclashfree.com/feed/v2ray-{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def nodefree():
+    today = datetime.today()
+    url = f"https://nodefree.githubrowcontent.com/{today.year}/{today.month:02d}/{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def nodeshare():
+    today = datetime.today()
+    url = f"https://a.nodeshare.xyz/uploads/{today.year}/{today.month:d}/{today.strftime('%Y%m%d')}.txt"
+    text = fetch_text(url)
+    return text
+
+
+def cczzuu():
+    today = datetime.today()
+    url = f"http://bikfx.cczzuu.top/node/{today.strftime('%Y%m%d')}-v2ray.txt"
+    text = fetch_text(url)
+    return text
+
+
+def oneclash():
+    today = datetime.today()
+    url = f"https://oneclash.githubrowcontent.com/{today.year}/{today.month:02d}/{today.strftime('%Y%m%d')}.txt"
     text = fetch_text(url)
     return text
 
@@ -56,10 +118,18 @@ def deduplicate(text):
 
 if __name__ == "__main__":
     text = ""
+    text += miluonode()
+    text += freeclash()
+    text += freeclashnode()
+    text += vpnlogin()
+    text += v2rayshareorg()
+    text += v2rayclashfree()
+    text += nodefree()
+    text += nodeshare()
+    text += cczzuu()
+    text += oneclash()
     text += v2rayshare()
     text += aiboboxx()
-    text += ermaozi()
     text = deduplicate(text)
-    print(text)
     text = base64.b64encode(text.encode('utf-8')).decode('utf-8')
     save_text(text, "v2ray.txt")
